2d69c4174ef0a95b547d2ebe3eaf5adbc82ecae8
Awijeet <avijit.shandilya@gmail.com>
2012-03-01 15:46:28 +0530

    Second

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Mobilecurry::Application.configure do
   config.active_support.deprecation = :notify
   config.assets.compile = true
   config.cache_store = :dalli_store
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
